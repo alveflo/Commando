5136c056c43ac5a474f566139d821a1b5b94d4f6
Victor Alveflo <victor.alveflo@gmail.com>
2016-09-05 21:40:49 +0200

    Figures added. Fixes #5

--- Commando/TestConsole/Program.cs
+++ Commando/TestConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Commando.Colors;
+using Commando.Figures;
 
 namespace TestConsole
 {
@@ -10,6 +11,20 @@ namespace TestConsole
             var Printer = new Printer();
             Printer.Red("? ").Blue("What is your favorite dish: ").NewLine().Green("This was cool!");
 
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine();
+            Console.WriteLine(Figure.Circle);
+            Console.WriteLine(Figure.CircleDotted);
+            Console.WriteLine(Figure.CircleFilled);
+            Console.WriteLine(Figure.ArrowDown);
+            Console.WriteLine(Figure.ArrowUp);
+            Console.WriteLine(Figure.ArrowLeft);
+            Console.WriteLine(Figure.ArrowRight);
+            Console.WriteLine(Figure.Hamburger);
+            Console.WriteLine(Figure.Pointer);
+            Console.WriteLine(Figure.PointerSmall);
+            Console.WriteLine(Figure.Smiley);
+            Console.WriteLine(Figure.Heart);
 
             Console.ReadLine();
         }
