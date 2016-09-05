d241e52f7553729be09b7d7313894da9ab94aa71
Victor Alveflo <victor.alveflo@gmail.com>
2016-09-05 20:51:39 +0200

    Color printer added. Fixes #2

---
+++ Commando/TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Commando.Colors;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Printer = new Printer();
+            Printer.Red("? ").Blue("What is your favorite dish: ").NewLine().Green("This was cool!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
